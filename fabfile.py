d550132334e483b27646133781faf33480a49d2f
Teodor Pripoae <toni@macbook.(none)>
2011-06-06 11:19:22 +0300

    Show assignee on tickets page

--- fabfile.py
+++ fabfile.py
@@ -8,9 +8,9 @@ def reboot_nginx():
     "Reboot nginx server."
     sudo("/etc/init.d/nginx restart", pty=True)
 
-def reboot_apache():
+def reboot_lighttpd():
     "Reboot apache server."
-    sudo("/etc/init.d/httpd reload", pty=True)
+    sudo("/etc/init.d/lighttpd reload", pty=True)
 
 def git_pull():
     "Updates the repository."
@@ -23,5 +23,5 @@ def db_migrate():
 def deploy():
     git_pull()
     db_migrate()
-    reboot_apache()
+    reboot_lighttpd()
     reboot_nginx()
